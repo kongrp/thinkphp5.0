1464394ade184baaa1ba35d528b46f7279695601
kongrp <960139253@qq.com>
2016-09-20 17:52:49 +0800

    3.1连接数据库

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,11 @@
 <?php
-namespace app\index\controller;
-
+namespace app\index\controller;   //命名空间，也说明了文件所在的位置
+use Think\Db;              		  //引用数据库操作类
+  
 class Index
 {
     public function index()
     {	
-    	$hello = 'Hello World!';
-        return $hello;
+    	var_dump(Db::name('teacher')->find());
     }
 }
